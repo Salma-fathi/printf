bbbd3b825fbf8d769bf74e1ec0d4d396bd850b48
Salma-fathi <salma89040@gmail.com>
2023-10-15 06:56:23 -0700

    bababba

--- printf_integer.c
+++ printf_integer.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * printf_integer - prints integer number
- * @args: number arguments
- * @printed: the printed characters
- * Return: printed characters
+ * print_integer - Prints an integer number
+ * @args: Number argument from va_list
+ * @printed: The number of characters printed so far
+ * Return: The updated number of printed characters
  */
-
-int printf_integer(va_list args, int printed)
+int print_integer(va_list args, int printed)
 {
     int num = va_arg(args, int);
     int digits = 0;
