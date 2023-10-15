309e212232e3175d6866aa57c20bbe8f3eeeef36
Salma-fathi <salma89040@gmail.com>
2023-10-15 06:37:29 -0700

    fhggg

--- printf_integer.c
+++ printf_integer.c
@@ -1,45 +1,52 @@
 #include "main.h"
 
 /**
- * printf_integer - prints intiger number
- * @args: number arguements
+ * printf_integer - prints integer number
+ * @args: number arguments
  * @printed: the printed characters
- * Return: printed charcaters
+ * Return: printed characters
  */
 
 int printf_integer(va_list args, int printed)
 {
-	int num = va_arg(args, int);
-	int digits = 0;
-	int temp = num;
-	int digit;
+    int num = va_arg(args, int);
+    int digits = 0;
+    int temp = num;
+    int digit;
 
-	if (num < 0)
-	{
-		printed += _putchar('-');
-		num = -num;
+    if (num < 0)
+    {
+        printed += _putchar('-');
+        temp = -num;
+    }
 
-		temp = num;
-	}
+    if (temp == 0)
+    {
+        printed += _putchar('0');
+        return printed;
+    }
 
-	do {
-		digits++;
-		temp /= 10;
-	} while (temp != 0);
+    while (temp != 0)
+    {
+        digits++;
+        temp /= 10;
+    }
 
-	while (digits > 0)
-	{
-		int pow10 = 1;
-		int i;
+    while (digits > 0)
+    {
+        int pow10 = 1;
+        int i;
 
-		for (i = 1; i < digits; i++)
-		{
-			pow10 *= 10;
-		}
-		digit = num / pow10;
-		printed += _putchar(digit + '0');
-		num -= digit * pow10;
-		digits--;
-	}
-	return (printed);
+        for (i = 1; i < digits; i++)
+        {
+            pow10 *= 10;
+        }
+
+        digit = num / pow10;
+        printed += _putchar(digit + '0');
+        num -= digit * pow10;
+        digits--;
+    }
+
+    return printed;
 }
