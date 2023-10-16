f38850bbdbaec024774d382c5c675d2f05808cca
Salma-fathi <salma89040@gmail.com>
2023-10-16 05:09:01 -0700

    lllf

--- _printf.c
+++ _printf.c
@@ -1,34 +1,74 @@
-#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+int _strlen(char *str);
 
 /**
- * _printf - implementation of the inbuilt printf
- * @format: the format specifier
- * Return: the formated string
+ * _printf - produces output according to a format
+ * @format: character string
+ *
+ * Return: number of characters printed excluding the null byte 
  */
 
 int _printf(const char *format, ...)
 {
-	int printed = 0;
-
 	va_list args;
+	/*char *str,*/
+      	char ch, *st;
+	int len;
 
 	va_start(args, format);
-
-	while (*format != '\0')
+	/*str = va_arg(args, char *);*/
+	len = 0;
+        while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			printed = selector(format, args, printed);
-			format++;
+			switch (*format)
+			{
+				case 'c':
+					ch = va_arg(args, int);
+					write (1, &ch, 1);
+					len++;
+					printf("length check c:%d\n", len);
+				       	break;
+				case 's':
+					st = va_arg(args, char *);
+					write (1, st, _strlen(st));
+					len = len + _strlen(st);
+					printf("length check s:%d\n", len);
+					break;
+				default:
+					break;
+			}
 		}
 		else
 		{
-			_putchar(*format);
-			printed++;
-			format++;
-		}
+			write (1, format, 1);
+			len++;
+			printf("length check else:%d\n", len);
+		}		
+		format++;
 	}
 	va_end(args);
-	return (printed);
+	return (len);
+}
+
+/**
+ * _strlen - finds string length
+ * @str: pointer to string
+ *
+ * Return: string length.
+ */
+
+int _strlen(char *str)
+{
+	int length = 0;
+	while (*str != '\0')
+	{
+		str++;
+		length++;
+	}
+	return (length);
 }
