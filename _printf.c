e414aaf4dbf6fb783e2ea0493fc7656355fd203f
Salma-fathi <salma89040@gmail.com>
2023-10-16 05:52:43 -0700

    mmnnb

--- _printf.c
+++ _printf.c
@@ -1,51 +1,67 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * _printf - formatted output conversion and print data.
- * @format: input string.
- *
- * Return: number of chars printed.
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and specifiers
+ * Return: number of characters printed (excluding the null byte)
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuff = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
+    va_list args;
+    int count = 0;
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
-		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == '\0')
-			{	print_buff(buffer, ibuff), free(buffer), va_end(arguments);
-				return (-1);
-			}
-			else
-			{	function = fetch_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					com_buff(buffer, format[i], ibuff), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuff);
-					i += id_print_func(format, i + 1);
-				}
-			} i++;
-		}
-		else
-			com_buff(buffer, format[i], ibuff), len++;
-		for (ibuff = len; ibuff > 1024; ibuff -= 1024)
-			;
-	}
-	print_buff(buffer, ibuff), free(buffer), va_end(arguments);
-	return (len);
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++; // Move past the '%'
+            if (*format == '\0')
+                break;
+
+            if (*format == 'c')
+            {
+                char c = va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                if (str == NULL)
+                    str = "(null)";
+                while (*str)
+                {
+                    write(1, str, 1);
+                    str++;
+                    count++;
+                }
+            }
+            else if (*format == '%')
+            {
+                write(1, "%", 1);
+                count++;
+            }
+            else
+            {
+                // Unknown specifier, print the character as is
+                write(1, "%", 1);
+                write(1, format, 1);
+                count += 2;
+            }
+        }
+        else
+        {
+            write(1, format, 1);
+            count++;
+        }
+
+        format++;
+    }
+
+    va_end(args);
+    return count;
 }
